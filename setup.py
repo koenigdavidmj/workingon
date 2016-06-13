898f1c70ad62138de72efd54c7e68821d02f8836
David Koenig <koenigdavidmj@gmail.com>
2016-06-12 22:21:50 -0700

    Minimal setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='workingon',
+      version='0.0.1',
+      description="Easy management of multiple-device development harnesses",
+      author='David Koenig',
+      author_email='koenigdavidmj@gmail.com',
+      scripts=['scripts/workingon'],
+)
